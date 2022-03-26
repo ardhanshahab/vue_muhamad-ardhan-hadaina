3d4c5307f4bd6823b2d022e84fb8db940ff5e6d3
ardhanshahab <ardhanshahab@gmail.com>
2022-03-26 14:45:11 +0700

    feat : summary vue helpers

---
+++ 12_VueCLI/todolistbig/src/components/theHasil.vue
@@ -0,0 +1,100 @@
+<template>
+<div>
+    <input type="text" class="baru" v-model="baru" @keyup.enter="weTambah" id="baru" >
+        <button v-if="!isEdit" @click="weTambah()">tambahkan</button>
+        <button v-else @click="saveEdit()">Save</button>
+    <div class="tabel">
+         <!-- <table v-if="todos.length > 0" class="tabelbodi">
+            <thead>
+                <td><h3>List yang harus dikerjakan</h3></td>
+            </thead>
+            <tbody v-for="(todo, index) in todos" :key="todo.id">
+                <td scope="row" class="tabelnomor">{{index + 1}}</td>
+                <td>{{todo.title}}</td>
+                <td><button @click="removeTask(index)">Hapus</button></td>
+                 <td><button @click="updateTask(index)" id="update">Edit</button></td>
+            </tbody>
+        </table> -->
+        <ol>
+          <li v-for="(todo, index) in todos" :key="index">
+            {{todo}}
+          <button @click="removeTask(index)">Hapus</button>
+          <button @click="updateTask(index)" id="update">Edit</button> 
+          </li>
+        </ol>
+        </div>
+        </div>
+</template>
+
+<script>
+
+export default {
+  name: 'theHasil',
+
+
+   data() {
+    return {
+      todos: [],
+        currentIndex: 0,
+        isEdit: false,
+        baru: ""
+    }
+  },
+  methods: {
+    weTambah() {
+      if (this.baru == ''){
+            alert("kosong")
+      }else{
+          this.todos.push(this.baru)
+        this.baru = ""
+      }
+    },
+     removeTask(index) {
+      this.todos.splice(index, 1)
+  },
+  updateTask(index) {
+    const currentValue = this.todos[index];
+    console.log(currentValue)
+    this.baru = currentValue;
+    this.isEdit = true;
+    this.currentIndex = index;
+      
+    },
+    saveEdit() {
+    this.todos.splice(this.currentIndex, 1, this.baru)
+    this.baru = "";
+    this.isEdit = false;
+    this.currentIndex = 0;
+            },
+    }
+  
+
+}
+
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.baru {
+  width: 80%;
+}
+.tabel {
+  justify-content: center;
+    text-align: center;
+  list-style-type: none;
+  padding: 20px 20px;
+  border: 10px;
+  border-color: black;
+  margin: 10px;
+
+}
+td {
+    text-align: center;
+  display: inline-block;
+  margin: 0 15px;
+}
+
+</style>
---
+++ 14_VueHelper/Summary.md
@@ -0,0 +1,11 @@
+# VUE Helper
+
+Navigasi adalah kegiatan berpindah dari satu halaman ke halaman lain
+navigasi ada 2 tipe 
+* navigasi beranak
+* navigasi dinamis
+
+layout template adalah komponen yang dapat dipakai sebagai susunan tata letak dasar yang membungkus halaman
+
+penyimpanan global adalah sebuah metode untuk menyimpan variable yang dapat diakses dengan mudah diseluruh aplikasi
+vuex adalah penyimpanan terpusat untukm semua komponen dalam aplikasi, dengan aturan yang memastikan bahwa variable yang disimpan hanya dapat dimutasi dengan cara terstruktur dan terprediksi
