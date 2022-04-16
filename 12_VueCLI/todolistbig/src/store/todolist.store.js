cc40c21fbf59f758d367ea62aeceb1211c70b3d0
ardhanshahab <ardhanshahab@gmail.com>
2022-04-16 20:59:12 +0700

    feat : todolist

--- 12_VueCLI/todolistbig/src/store/todolist.store.js
+++ 12_VueCLI/todolistbig/src/store/todolist.store.js
@@ -1,16 +1,25 @@
 const state = {
  todos: []
-
 }
 
 const mutations = {
-
-    
+      setTodoList(state, param) {
+        state.todos = param;
+      }
 }
 
 const actions = {
+    insertTodo(store, param) {
+        const todos = store.state.todos;
 
-}
+        todos.push({
+          todos: param.todos
+        });
+
+
+        store.commit("setTodoList", todos);
+      }
+};
 
 export default {
     state,
--- 12_VueCLI/todolistbig/src/views/theHasil.vue
+++ 12_VueCLI/todolistbig/src/views/theHasil.vue
@@ -3,30 +3,19 @@
   <h1>TODOLIST</h1>
     <input type="text" class="baru" v-model="baru" @keyup.enter="weTambah" id="baru" >
         <button v-if="!isEdit" @click="weTambah()">tambahkan</button>
-        <!-- <button v-else @click="saveEdit()">Save</button> -->
+        <button v-else @click="saveEdit()">Save</button>
     <div class="tabel">
         <ol>
           <li v-for="(todo, index) in todos" 
           :key="index"
           @click="redirect(todo)" >
             {{todo}}
-          <!-- <button @click="removeTask(index)">Hapus</button>
-          <button @click="updateTask(index)" id="update">Edit</button>  -->
+          <button @click="removeTask(index)">Hapus</button>
+          <button @click="updateTask(index)" id="update">Edit</button> 
           </li>
         </ol>
         </div>
-        <hr/>
-        <input type="text" v-model="inputBrand" placeholder="Brand" />
-    <br />
-    <input type="text" v-model="inputPrice" placeholder="Price" />
-    <br /><br />
-    <button @click="pushLaptop">Push User</button>
-        <li v-for="(laptop, index) in laptopList" :key="index">
-        Brand: {{ laptop.brand }}
-        <br />
-        Price: Rp. {{ laptop.price }}
-      </li>
-        </div>
+</div>
 </template>
 
 <script>
@@ -35,58 +24,57 @@ export default {
 
 
    data() {
-    // return {
-    //   todos: [],
-    //     currentIndex: 0,
-    //     isEdit: false,
-    //     baru: "", 
-    // }
+    return {
+      todos: [],
+        currentIndex: 0,
+        isEdit: false,
+        baru: "", 
+    }
   },
   
   computed: {
-    computetodo() {
-      return this.$store.state.todolist.todo;
+    computedtodo() {
+      return this.$store.state.todolist.todos;
     },
     
   },
   
-  // methods: {
-  //   redirect(todo){
-  //     return this.$router.push({ name: 'taskDetail', params: {item: todo},
-  //   })
-  //   },
+  methods: {
+    redirect(todo){
+      return this.$router.push({ name: 'taskDetail', params: {item: todo},
+    })
+    },
  
-  //   weTambah() {
-  //     if (this.baru == ''){
-  //           alert("kosong")
-  //     }else{
-  //       const todo = this.baru
-  //       // this.$store.dispatch("todolist/setList",
-  //       // {
-        
-  //       // name: todo},
-  //       console.log("good", todo)
-  //       //,)
-  //       }
-  //   },
-  //    removeTask(index) {
-  //     this.todos.splice(index, 1)
-  // },
-  // updateTask(index) {
-  //   const currentValue = this.todos[index];
-  //   console.log(currentValue)
-  //   this.baru = currentValue;
-  //   this.isEdit = true;
-  //   this.currentIndex = index;
+    weTambah() {
+
+            if(this.baru == ''){
+              alert("kosong");
+            }
+            else{
+                this.todos.push(this.baru);
+                this.baru = "";
+
+            
+        }
+    },
+     removeTask(index) {
+      this.todos.splice(index, 1)
+  },
+  updateTask(index) {
+    const currentValue = this.todos[index];
+    console.log(currentValue)
+    this.baru = currentValue;
+    this.isEdit = true;
+    this.currentIndex = index;
       
-  //   },
-  //   saveEdit() {
-  //   this.todos.splice(this.currentIndex, 1, this.baru)
-  //   this.baru = "";
-  //   this.isEdit = false;
-  //   this.currentIndex = 0;
-  //           },
-  //   }
+    },
+    saveEdit() {
+    this.todos.splice(this.currentIndex, 1, this.baru)
+    this.baru = "";
+    this.isEdit = false;
+    this.currentIndex = 0;
+            },
+    }
   
 
 }
